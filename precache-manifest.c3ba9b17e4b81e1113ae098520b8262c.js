d494f9702dd13639843e31c87908a4a992c3c80d
EC2 Default User <ec2-user@ip-172-31-41-28.us-east-2.compute.internal>
2019-06-26 18:36:31 +0000

    Updates

---
+++ precache-manifest.c3ba9b17e4b81e1113ae098520b8262c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578d162b3e3cf63212b551da19d66ffa",
+    "url": "/Recipe_App/index.html"
+  },
+  {
+    "revision": "d28914408ed3cbb07c72",
+    "url": "/Recipe_App/static/css/main.e153c91e.chunk.css"
+  },
+  {
+    "revision": "0318eb5aadc6bb66851b",
+    "url": "/Recipe_App/static/js/2.ce2d1ea8.chunk.js"
+  },
+  {
+    "revision": "d28914408ed3cbb07c72",
+    "url": "/Recipe_App/static/js/main.e4433077.chunk.js"
+  },
+  {
+    "revision": "2dba645b99cf50976454",
+    "url": "/Recipe_App/static/js/runtime~main.d25c7b83.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Recipe_App/precache-manifest.c3ba9b17e4b81e1113ae098520b8262c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Recipe_App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
